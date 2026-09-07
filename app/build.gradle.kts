a0638d7a181f97df448f351d65d57148034db8a3
takeda-akihiro <akihirotakeda1111@gmail.com>
2026-09-07 09:48:48 +0900

    release v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.example.twotouchkeyboard"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         buildConfigField("String", "UPDATE_REPOSITORY", "\"$updateRepository\"")
     }
